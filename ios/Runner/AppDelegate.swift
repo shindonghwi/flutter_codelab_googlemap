96599de420a269eb6e876460de1eb259190e85f6
shindonghwi <ehdgnl8940@naver.com>
2022-09-16 12:13:33 +0900

    fix: googlemap setting

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import Flutter
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    GMSServices.provideAPIKey("AIzaSyCcPvRWPbhEx2AEFY9UNOX0hu2ggHahz9E")
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
